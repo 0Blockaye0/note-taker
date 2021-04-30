ec0cac5f084f1bc8810d94e742bc1848797a56c6
Blake Johnson <blakejohnson@Blakes-MBP.attlocal.net>
2021-04-30 17:18:12 -0500

    post route and write file to db complete

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,10 +1,30 @@
-const { json } = require("body-parser");
+const { json, text } = require("body-parser");
 const path = require("path");
 const router = require("express").Router();
 const savedNotes = require("../../db/db.json");
+const fs = require("fs");
 
-router.get("/savedNotes", (req, res) => {
+function createNewNote(body, notesArr) {
+  const newNote = body;
+  notesArr.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify(notesArr)
+  );
+  return newNote
+};
+
+// ROUTE FOR GETTING SAVED NOTES
+router.get("/notes", (req, res) => {
   res.json(savedNotes);
+});
+
+// i want to take an incoming new note and add it to the db json obj (and giving it a unique id), then return the new note
+router.post("/notes", (req, res) => {
+  let newNote = req.body;
+  console.log(savedNotes)
+  const savedNote = createNewNote(newNote, savedNotes);
+  res.json(savedNote); 
 });
 
 module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -10,3 +10,4 @@ router.get("/notes", (req, res) => {
 });
 
 module.exports = router;
+
