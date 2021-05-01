d2a61a0ce75cec2897ba37e762012a49e451d860
Blake Johnson <blakejohnson@Blakes-MBP.attlocal.net>
2021-05-01 14:19:21 -0500

    Delete route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 let savedNotes = require("../../db/db.json");
 const fs = require("fs");
 const cuid = require("cuid");
+// const renderNoteList = require("../../public/assets/js/index");
 
 // MAKES NEW NOTE FROM REQ BODY AND WRITES IT TO DB/ RETURNS NEW NOTE
 function createNewNote(body, notesArr) {
@@ -30,15 +31,17 @@ router.post("/notes", (req, res) => {
   res.json(savedNote); 
 });
 
+// DELETES A NOTE BY ID
 router.delete("/notes/:id", (req, res) => {
-  console.log(savedNotes);
+  // console.log(savedNotes);
     let peram = req.params;
     let newSavedNotes = savedNotes.filter(note => note.id != peram.id)
     // someArray = someArray.filter(person => person.name != 'John');
     // console.log(newSavedNotes);
     savedNotes = newSavedNotes;
+    // renderNoteList();
     return savedNotes;
   // delete savedNotes;
-})
+});
 
 module.exports = router;
